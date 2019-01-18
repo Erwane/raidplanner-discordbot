30ea4021df0fb27169231dfc774db2c8a7af3f68
Erwane Breton <erwane@phea.fr>
2019-01-18 15:02:29 +0100

    reaction class debug on old message react

---
+++ src/reaction.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+
+class Reaction:
+    def __init__(self, discordClient):
+        self.client = discordClient
+        self.no = "🚫"
+        self.yes = "✅"
+        self.maybe = "❓"
+
+    # 308664314993180673 react on 535753694982176789 with reaction: ✅
+    async def on(self, payload):
+        # ignore myself
+        if payload.user_id == self.client.user.id:
+            return
+
