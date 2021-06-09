30254776d7d7e112b409d6ca7a810cb86de04586
Erwane Breton <erwane@phea.fr>
2021-06-09 14:14:27 +0200

    setup: clean code

--- src/setup.py
+++ src/setup.py
@@ -2,10 +2,8 @@
 
 from .mylibs import log
 import asyncio
-import discord
-from discord.ext import commands
 import re
-from pprint import pprint
+
 
 class Setup:
     def __init__(self, bot):
@@ -23,16 +21,16 @@ class Setup:
         author = msg.author
 
         # Linked user ?
-        raidplannerAuthor = await self.bot.getRaidplannerUser(author, True)
+        raidplanner_author = await self.bot.getRaidplannerUser(author, True)
 
-        if not raidplannerAuthor:
+        if not raidplanner_author:
             return False
 
         # guild
         guild = msg.guild
-        raidplannerGuild = self.db.getGuild(guild.id)
+        raidplanner_guild = self.db.getGuild(guild.id)
 
-        if raidplannerGuild:
+        if raidplanner_guild:
             await msg.channel.send(f"Le serveur discord **{guild.name}** est déjà lié au Raidplanner.")
             return True
 
@@ -47,7 +45,7 @@ Vous trouverez ce token comme ceci :
 
         counter = 0
         while counter < 3:
-            status, raidplannerGuild = await self._attachAskToken(author, guild)
+            status, raidplanner_guild = await self.__ask_token(author, guild)
 
             if status == 'not_found':
                 counter += 1
@@ -55,40 +53,41 @@ Vous trouverez ce token comme ceci :
             elif status == 'already_attached':
                 return await author.send(f"Désolé, ce token est déjà utilisé sur un autre serveur discord.")
             elif status == 'attached':
-                self.api.discordAttach(author, guild, raidplannerGuild)
+                self.api.discordAttach(author, guild, raidplanner_guild)
                 await author.send("Token validé")
-                return await msg.channel.send(f"Merci, votre serveur discord **{guild.name}** est maintenant lié au Raidplanner.")
+                return await msg.channel.send(
+                    f"Merci, votre serveur discord **{guild.name}** est maintenant lié au Raidplanner.")
             else:
                 counter = 10
 
         await author.send("Session terminé.")
         await msg.channel.send(f"Le serveur discord **{guild.name}** n'a pas été lié au Raidplanner.")
 
-    async def _attachAskToken(self, author, guild):
+    async def __ask_token(self, author, guild):
         await author.send("Veuillez saisir votre token de guilde.")
 
-        def checkToken(m):
+        def check_token(m):
             return re.match("^[A-Za-z0-9]{32}$", m.content.strip())
 
         try:
             # wait for owner reply
-            reply = await self.client.wait_for('message', timeout=10.0, check=checkToken)
+            reply = await self.client.wait_for('message', timeout=10.0, check=check_token)
 
             # get raiplanner guild
-            raidplannerGuild = self.db.getGuild(guild.id, reply.content.strip())
+            raidplanner_guild = self.db.getGuild(guild.id, reply.content.strip())
 
-            if raidplannerGuild:
-                if raidplannerGuild['id'] == guild.id:
-                    return 'attached', raidplannerGuild
+            if raidplanner_guild:
+                if raidplanner_guild['id'] == guild.id:
+                    return 'attached', raidplanner_guild
                 else:
                     return 'already_attached', False
 
             return 'not_found', False
-        except asyncio.TimeoutError as e:
+        except asyncio.TimeoutError:
             return 'timeout', False
 
     """
-    detach Raidplanner from discord
+    Detach bot from discord
     """
     async def detach(self, msg):
         if not await self.bot.checkServerOwner(msg, True):
@@ -96,31 +95,33 @@ Vous trouverez ce token comme ceci :
 
         # author
         author = msg.author
-        raidplannerAuthor = await self.bot.getRaidplannerUser(author, True)
-        if not raidplannerAuthor:
+        raidplanner_author = await self.bot.getRaidplannerUser(author, True)
+        if not raidplanner_author:
             return False
 
         # guild
         guild = msg.guild
-        raidplannerGuild = self.db.getGuild(guild.id)
+        raidplanner_guild = self.db.getGuild(guild.id)
 
-        if not raidplannerGuild:
+        if not raidplanner_guild:
             await msg.channel.send(f"Le serveur discord **{guild.name}** n'est pas lié au Raidplanner.")
             return True
 
         # Wait owner response
         try:
-            await msg.channel.send(f"Le bot sera détaché de **{guild.name}** et toutes les données du bot seront supprimées. **Êtes vous sur ?** (`Y|Yes|O|Oui` / `N|No|Non`)")
+            await msg.channel.send(
+                f"Le bot sera détaché de **{guild.name}** et toutes les données du bot seront supprimées. **Êtes vous "
+                f"sur ?** (`Y|Yes|O|Oui` / `N|No|Non`)")
             reply = await self.client.wait_for('message', timeout=5.0)
             response = reply.content.strip()
             if re.match("^Y|Yes|O|Oui$", response, flags=re.IGNORECASE):
                 # Detach bot
-                self.api.discordDetach(author, guild, raidplannerGuild)
+                self.api.discordDetach(author, guild, raidplanner_guild)
                 self.db.detachBot(guild.id)
                 await msg.channel.send(f"Le serveur discord **{guild.name}** n'est plus lié au bot.")
             else:
                 await msg.channel.send("Ouf :)")
-        except Exception as e:
+        except Exception:
             await msg.channel.send("Ouf :)")
 
     # assign an events channel for bot
@@ -134,9 +135,9 @@ Vous trouverez ce token comme ceci :
         guild = msg.guild
         channel = msg.channel
 
-        guildToken = self.db.getTokenGuild(guild.id)
+        guild_token = self.db.getTokenGuild(guild.id)
 
-        if not guildToken:
+        if not guild_token:
             await channel.send("""Vous devez attacher ce serveur discord au bot avec la commande `!rp attach`""")
             return False
 
@@ -157,66 +158,68 @@ Pour que le service fonctionne bien, voici les droits requis dans ce canal :
 ```
 """)
 
-        async def ask(author, guild):
-            def checkAnswer(m):
-                match = re.match("^<#(\d+)>", m.content.strip())
-                if not match:
+        async def ask():
+            def check_answer(m):
+                matches = re.match("^<#([0-9]+)>", m.content.strip())
+                if not matches:
                     return False
 
                 for chan in m.guild.channels:
-                    if chan.id == int(match.group(1)):
+                    if chan.id == int(matches.group(1)):
                         return chan.id
 
                 return False
 
             try:
-                reply = await self.client.wait_for('message', timeout=120.0, check=checkAnswer)
+                reply = await self.client.wait_for('message', timeout=120.0, check=check_answer)
 
                 # extract and return channel id
-                match = re.match("^<#(\d+)>", reply.content.strip())
+                match = re.match("^<#([0-9]+)>", reply.content.strip())
 
                 return int(match.group(1))
-            except asyncio.TimeoutError as e:
+            except asyncio.TimeoutError:
                 return 'timeout'
 
         counter = 0
         while counter < 3:
-            channelId = await ask(author, guild)
+            channel_id = await ask()
 
-            if channelId > 0:
+            if channel_id > 0:
                 try:
-                    botChannel = guild.get_channel(channelId)
-                    permissions = guild.me.permissions_in(botChannel)
+                    bot_channel = guild.get_channel(channel_id)
+                    permissions = guild.me.permissions_in(bot_channel)
 
-                    missingPermissions = []
+                    missing_permissions = []
 
                     if not permissions.read_messages:
-                        missingPermissions.append("**Lecture** : Lire les messages")
+                        missing_permissions.append("**Lecture** : Lire les messages")
 
                     if not permissions.send_messages:
-                        missingPermissions.append("**Ecriture** : Envoyer des messages")
+                        missing_permissions.append("**Écriture** : Envoyer des messages")
 
                     if not permissions.manage_messages:
-                        missingPermissions.append("**Manage** : Gérer les messages")
+                        missing_permissions.append("**Manage** : Gérer les messages")
 
                     if not permissions.read_message_history:
-                        missingPermissions.append("**Historique** : Voir les anciens messages")
+                        missing_permissions.append("**Historique** : Voir les anciens messages")
 
                     if not permissions.mention_everyone:
-                        missingPermissions.append("**Mention** : Mentionner `@everyone` (n'utilise que `@here`)")
+                        missing_permissions.append("**Mention** : Mentionner `@everyone` (n'utilise que `@here`)")
 
                     if not permissions.add_reactions:
-                        missingPermissions.append("**Réaction** : Ajouter des réactions")
+                        missing_permissions.append("**Réaction** : Ajouter des réactions")
 
-                    if len(missingPermissions):
-                        msg = f"Désolé, il me manque les permissions suivantes dans le canal <#{channelId}>."
-                        return await channel.send(msg + "\n- " + "\n- ".join(missingPermissions))
+                    if len(missing_permissions):
+                        msg = f"Désolé, il me manque les permissions suivantes dans le canal <#{channel_id}>."
+                        return await channel.send(msg + "\n- " + "\n- ".join(missing_permissions))
 
                     # attach channel id to guild id
-                    self.db.query('UPDATE guilds SET channel=? WHERE id=?', channelId, guild.id)
-                    log().info(f"{author.name}#{author.discriminator} give bot channel #{botChannel.name}<{botChannel.id}> in guild <{guild.id}>")
+                    self.db.query('UPDATE guilds SET channel=? WHERE id=?', channel_id, guild.id)
+                    log().info(
+                        f"{author.name}#{author.discriminator} give bot channel #{bot_channel.name}<{bot_channel.id}> "
+                        f"in guild <{guild.id}>")
 
-                    return await channel.send(f"Merci, je publierai les événements dans le canal <#{channelId}>.")
+                    return await channel.send(f"Merci, je publierai les événements dans le canal <#{channel_id}>.")
                 except Exception as e:
                     log().error(str(e))
             else:
@@ -235,28 +238,29 @@ Pour que le service fonctionne bien, voici les droits requis dans ce canal :
         guild = msg.guild
         channel = msg.channel
 
-        await channel.send("""Publier les événements qui ont lieu dans combien de jours ? (saisir un nombre entre **1** et **15**)""")
+        await channel.send(
+            """Publier les événements qui ont lieu dans combien de jours ? (saisir un nombre entre **1** et **15**)""")
 
         async def ask():
-            def checkAnswer(m):
-                match = re.match("^(\d+)$", m.content.strip())
+            def check_answer(m):
+                match = re.match("^([0-9]+)$", m.content.strip())
                 if match:
                     d = int(match.group(1))
-                    if d >= 1 and d <= 15:
+                    if 1 <= d <= 15:
                         return True
 
                 return False
 
             try:
-                reply = await self.client.wait_for('message', timeout=30.0, check=checkAnswer)
+                reply = await self.client.wait_for('message', timeout=30.0, check=check_answer)
 
                 return int(reply.content.strip())
-            except asyncio.TimeoutError as e:
+            except asyncio.TimeoutError:
                 return False
 
         days = await ask()
 
-        if days != False:
+        if days:
             self.db.query('UPDATE guilds SET event_days=? WHERE id=?', days, guild.id)
             await channel.send("""C'est enregistré.""")
         else:
